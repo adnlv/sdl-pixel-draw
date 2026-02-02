50d497c0f9c8ac3e20bc689b737997472f07f2c1
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-02-02 18:53:44 +0200

    Roll back to single `main` function

--- src/main.c
+++ src/main.c
@@ -1,37 +1,17 @@
 #include <stdlib.h>
-
-#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
 #include <SDL3/SDL_render.h>
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
-
-static void renderText(void *appstate) {
-    const char *message = "Welcome to Pixel Draw";
-    const float scale = 4.0f;
-    int w = 0, h = 0;
-
-    SDL_GetCurrentRenderOutputSize(renderer, &w, &h);
-    SDL_SetRenderScale(renderer, scale, scale);
-
-    const float x = (w / scale - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    const float y = (h / scale - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xFF);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-    SDL_RenderDebugText(renderer, x, y, message);
-    SDL_RenderPresent(renderer);
-}
-
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+static int init() {
     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
         SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
+    return SDL_APP_CONTINUE;
+}
+
+static int create_window_and_renderer(SDL_Window **window, SDL_Renderer **renderer) {
     const int width = 720;
     const int height = 480;
     const char title[] = "Pixel Draw";
@@ -39,7 +19,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
                                   SDL_WINDOW_INPUT_FOCUS |
                                   SDL_WINDOW_HIGH_PIXEL_DENSITY;
 
-    if (!SDL_CreateWindowAndRenderer(title, width, height, flags, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer(title, width, height, flags, window, renderer)) {
         SDL_Log("Failed to create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -47,20 +27,47 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     return SDL_APP_CONTINUE;
 }
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;
+static void destroy_window_and_renderer(SDL_Window *window, SDL_Renderer *renderer) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+}
+
+static void quit() {
+    SDL_Quit();
+}
+
+int main(int argc, char *argv[]) {
+    int status = 0;
+    int is_running = true;
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Event event;
+
+    status = init();
+    if (status != SDL_APP_CONTINUE) {
+        return status;
     }
 
-    return SDL_APP_CONTINUE;
+    status = create_window_and_renderer(&window, &renderer);
+    if (status != SDL_APP_CONTINUE) {
+        return status;
+    }
+
+    while (is_running) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    is_running = false;
+                    break;
+                default: ;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    destroy_window_and_renderer(window, renderer);
+    quit();
 }
-
-SDL_AppResult SDL_AppIterate(void *appstate) {
-    renderText(appstate);
-
-    return SDL_APP_CONTINUE;
-}
-
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-}
-
