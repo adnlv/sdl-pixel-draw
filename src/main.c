f5e0ba027e9fdb214310fadd134d6b21ca0bb0db
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-02-02 16:45:47 +0200

    Use SDL callbacks

--- src/main.c
+++ src/main.c
@@ -1,55 +1,39 @@
 #include <stdlib.h>
+
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include <SDL3/SDL_render.h>
 
-static void init(void) {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("failed to initialize SDL: %s", SDL_GetError());
-        exit(SDL_APP_FAILURE);
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
-}
 
-static void quit(SDL_Window *window, SDL_Renderer *renderer) {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-static void create_window_and_renderer(SDL_Window **window, SDL_Renderer **renderer) {
-    if (!SDL_CreateWindowAndRenderer("Pixel Draw", 720, 480, SDL_WINDOW_INPUT_FOCUS, window, renderer)) {
-        SDL_Log("failed to create window and renderer: %s", SDL_GetError());
-        exit(SDL_APP_FAILURE);
-    }
-}
-
-static int handle_event(const SDL_Event *event) {
-    switch (event->type) {
-        case SDL_EVENT_QUIT:
-            return SDL_APP_SUCCESS;
-        default: ;
+    if (!SDL_CreateWindowAndRenderer("Pixel Draw", 720, 480, SDL_WINDOW_INPUT_FOCUS, &window, &renderer)) {
+        SDL_Log("Failed to create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
     return SDL_APP_CONTINUE;
 }
 
-int main(int argc, char *argv[]) {
-    init();
-
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-
-    create_window_and_renderer(&window, &renderer);
-
-    SDL_Event event;
-    int is_running = true;
-    while (is_running) {
-        while (SDL_PollEvent(&event)) {
-            if (handle_event(&event) != SDL_APP_CONTINUE) {
-                is_running = false;
-                break;
-            }
-        }
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
     }
 
-    quit(window, renderer);
+    return SDL_APP_CONTINUE;
 }
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+}
+
