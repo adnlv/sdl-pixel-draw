d55424fa2f07ae09f92dd6a217c5564a2ea2dd12
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-02-02 15:15:14 +0200

    Make pointer arguments immutable

--- src/storage/binary_file_storage.c
+++ src/storage/binary_file_storage.c
@@ -1,17 +1,17 @@
 #include "binary_file_storage.h"
 
-FILE *open_binary_file(const char *filename) {
+FILE *open_binary_file(const char *const filename) {
     return fopen(filename, "wb+");
 }
 
-int close_binary_file(FILE *file) {
+int close_binary_file(FILE *const file) {
     return fclose(file);
 }
 
-uint16_t save_pixel_buffer_as_binary_file(const pixel_t *pixels,
+uint16_t save_pixel_buffer_as_binary_file(const pixel_t *const pixels,
                                           const uint8_t width,
                                           const uint8_t height,
-                                          FILE *file) {
+                                          FILE *const file) {
     uint16_t items = 0;
     fseek(file, 0, SEEK_SET);
     items += fwrite(&width, sizeof(uint8_t), 1, file);
@@ -20,10 +20,10 @@ uint16_t save_pixel_buffer_as_binary_file(const pixel_t *pixels,
     return items;
 }
 
-uint16_t read_pixel_buffer_from_binary_file(pixel_t *pixels,
-                                            uint8_t *width,
-                                            uint8_t *height,
-                                            FILE *file) {
+uint16_t read_pixel_buffer_from_binary_file(pixel_t *const pixels,
+                                            uint8_t *const width,
+                                            uint8_t *const height,
+                                            FILE *const file) {
     uint16_t items = 0;
     fseek(file, 0, SEEK_SET);
     items += fread(width, sizeof(uint8_t), 1, file);
